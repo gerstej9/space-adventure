831239315909824007b42f8fc74e341b6f7dd41f
tekthree <tekthree@gmail.com>
2020-11-17 11:12:57 -0800

    adding popup modals

---
+++ js/homepage.js
@@ -0,0 +1,30 @@
+'use strict'
+
+
+
+
+
+var modal = document.getElementById("myModal");
+
+// Get the label that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the label, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of mogal content close it up 
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}

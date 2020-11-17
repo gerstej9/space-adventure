9dabd0dbc8663f05471694665c041cff676ebede
tekthree <tekthree@gmail.com>
2020-11-17 13:52:40 -0800

    content connected to modals

--- js/homepage.js
+++ js/homepage.js
@@ -4,82 +4,82 @@
 
 
 
-var modal = document.getElementById("myModalOne");
+var modal1 = document.getElementById("myModalOne");
 
 // Get the label that opens the modal
-var btn = document.getElementById("myBtnOne");
+var btn1 = document.getElementById("myBtnOne");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
 
 // When the user clicks on the label, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
+btn1.onclick = function() {
+  modal1.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-  modal.style.display = "none";
+  modal1.style.display = "none";
 }
 
 // When the user clicks anywhere outside of mogal content close it up 
 window.onclick = function(event) {
   if (event.target == modal) {
-    modal.style.display = "none";
+    modal1.style.display = "none";
   }
 }
 // ---------------------- second modal leaderboard ------------------------ 
 
-var modal = document.getElementById("myModalTwo");
+var modal2 = document.getElementById("myModalTwo");
 
 // Get the label that opens the modal
-var btn = document.getElementById("myBtnTwo");
+var btn2 = document.getElementById("myBtnTwo");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("closeA")[0];
 
 
 // When the user clicks on the label, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
+btn2.onclick = function() {
+  modal2.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-  modal.style.display = "none";
+  modal2.style.display = "none";
 }
 
 // When the user clicks anywhere outside of mogal content close it up 
 window.onclick = function(event) {
   if (event.target == modal) {
-    modal.style.display = "none";
+    modal2.style.display = "none";
   }
 }
 // ---------------------- second modal leaderboard ------------------------ 
 
-var modal = document.getElementById("myModalThree");
+var modal3 = document.getElementById("myModalThree");
 
 // Get the label that opens the modal
-var btn = document.getElementById("myBtnThree");
+var btn3 = document.getElementById("myBtnThree");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("closeB")[0];
 
 
 // When the user clicks on the label, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
+btn3.onclick = function() {
+  modal3.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-  modal.style.display = "none";
+  modal3.style.display = "none";
 }
 
 // When the user clicks anywhere outside of mogal content close it up 
 window.onclick = function(event) {
   if (event.target == modal) {
-    modal.style.display = "none";
+    modal3.style.display = "none";
   }
 }
